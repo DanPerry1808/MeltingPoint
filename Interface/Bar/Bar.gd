023281afde4fe19ba7b278cf6db16a5d07364f76
adszads <51888045+adszads@users.noreply.github.com>
2020-02-29 16:31:44 +0000

    merged

--- Interface/Bar/Bar.gd
+++ Interface/Bar/Bar.gd
@@ -1,7 +1,7 @@
 extends HBoxContainer
 
 func _ready():
-	var player = get_parent().get_parent().get_parent().get_parent().get_node("Grid").get_node("Player")
+	var player = get_parent().get_parent().get_node("Grid").get_node("Player")
 	print(player.name)
 	player.connect("temp_update", self, "_on_temp_update")
 	pass
