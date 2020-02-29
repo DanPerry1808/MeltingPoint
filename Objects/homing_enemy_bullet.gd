93686b37b152a863789df4b6dd6fed42a8ad737d
adszads <51888045+adszads@users.noreply.github.com>
2020-02-29 16:06:28 +0000

    added homing bullets

---
+++ Objects/homing_enemy_bullet.gd
@@ -0,0 +1,10 @@
+extends "res://Objects/enemy_bullet.gd"
+
+onready var player = get_parent().get_parent().get_node('Player')
+var homing_amount = .1
+
+func move(dt):
+	var player_pos = player.get_position()
+	var diff = (player_pos - get_position()).normalized()
+	delta = (delta + diff * homing_amount).normalized()
+	set_position(get_position() + delta * speed * dt)
---
+++ pawns/missile_shoot_enemy.gd
@@ -0,0 +1,6 @@
+extends "res://pawns/shoot_enemy.gd"
+
+
+func _ready():
+	Bullet = preload("res://Objects/HomingEnemyBullet.tscn")
+	distance = 8
